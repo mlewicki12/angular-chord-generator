22e5defaff2637d99c36b7e7fc29328d8b43c3f5
Maciej Lewicki <lewickimapl@gmail.com>
2021-04-18 21:00:27 +0200

    create chord component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChordComponent } from './components/chord/chord.component';
+import { ChordlistComponent } from './components/chordlist/chordlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChordComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/chord/chord.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chord',
+  templateUrl: './chord.component.html',
+  styleUrls: ['./chord.component.scss']
+})
+export class ChordComponent implements OnInit {
+  strings: string[];
+  chord: number[];
+
+  getChord(index: number): string {
+    return `${this.chord[index]}`;
+  }
+
+  constructor() {
+    this.strings = ['E', 'B', 'G', 'D', 'A', 'E']; 
+    this.chord = [0, 1, 0, 2, 3, -1];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
